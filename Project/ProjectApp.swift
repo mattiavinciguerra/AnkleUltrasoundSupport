2e924e701c8c6c30c35506c31133751f57d928a8
Mattia Vinciguerra <mattia.vinciguerra@studenti.unimi.it>
2024-07-04 18:13:36 +0200

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Mattia Vinciguerra on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
